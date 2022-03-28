4d23a20326aa1e37e8c8a4db8d25ccfcd1edefae
Matthew Faigan <matthew.faigan@gmail.com>
2022-03-28 18:29:37 -0400

    Implement page functions

---
+++ page.cpp
@@ -0,0 +1,16 @@
+#include "include/page.h"
+
+Page::Page(std::string id, unsigned int value)
+    : pair(std::make_pair(id, value))
+{
+}
+
+std::string Page::getId() const
+{
+    return std::get<0>(pair);
+}
+
+unsigned int Page::getValue() const
+{
+    return std::get<1>(pair);
+}
