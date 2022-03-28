fed557fbdc9b2f23f2309fb0f51aff8cb32b1bee
Matthew Faigan <matthew.faigan@gmail.com>
2022-03-28 12:08:44 -0400

    Reuse clock from A2 with minor modifications

---
+++ clock.cpp
@@ -0,0 +1,40 @@
+#include "include/clock.h"
+
+#include <exception>
+#include <thread>
+#include <mutex>
+#include <iostream>
+
+Clock& Clock::getInstance()
+{
+    // The static instance persists between calls, so the first getInstance() call will create the instance and future calls will return the same instance.
+    static Clock instance;
+    return instance;
+}
+
+Clock::Clock()
+    : time(0), started(false)
+{
+}
+
+int Clock::getTime() const
+{
+    // Get a shared lock allowing any number of readers, as long as nobody is writing
+    std::shared_lock readerLock(mutex);
+    return time;
+}
+
+void Clock::start()
+{
+    if (started) throw std::runtime_error("Clock has already been started");
+
+    started = true;
+    for (unsigned int count = 0; count < maxCycles; count++)
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(cycleLength));
+        // Get a unique lock that blocks all readers while the increment happens
+        std::unique_lock writerLock(mutex);
+        time++;
+        std::cout << time << std::endl;
+    }
+}
---
+++ include/clock.h
@@ -0,0 +1,31 @@
+#ifndef CLOCK_H
+#define CLOCK_H
+
+#include <shared_mutex>
+
+// Simulates the clock used by the OS when scheduling processes. Thread-safe, uses the singleton pattern to share one instance through getInstance().
+// Singleton pattern based on: https://stackoverflow.com/a/1008289
+class Clock
+{
+public:
+    static constexpr unsigned int maxCycles = 6000U;
+    static constexpr unsigned int cycleLength = 50U;
+    static constexpr unsigned int pollingInterval = 10U;
+
+    static Clock& getInstance();
+
+public:
+    Clock(const Clock& other) = delete;
+    void operator=(const Clock& other) = delete;
+
+    int getTime() const;
+    void start();
+
+private:
+    Clock();
+    mutable std::shared_mutex mutex;
+    int time;
+    bool started;
+};
+
+#endif
