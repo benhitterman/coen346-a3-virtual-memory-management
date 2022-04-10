238d5e534a7887a5b37394b906d48b87f08fa203
alexsantelli <santellialex@gmail.com>
2022-04-10 11:09:45 -0400

    Rading from Nextcommand

--- main.cpp
+++ main.cpp
@@ -19,18 +19,12 @@ int main()
     size_t maxPages, k;
     int timeout;
     myfile >> maxPages >> k >> timeout;
-    MemoryManager memory(maxPages, k, timeout);
+    MemoryManager *memory = new MemoryManager(maxPages, k, timeout);
     myfile.close();
 
     // Reading Commands text
-    myfile.open("commands.txt");
-    if (!myfile)
-    {
-        std::cout << "Can not open file.";
-        exit(1);
-    }
-
-    myfile.close();
+    nextCommand *commands = new nextCommand();
+    commands->findingListOfCommand("commands.txt");
 
     // Reading list of Processes
     myfile.open("processes.txt");
@@ -49,7 +43,7 @@ int main()
     for (int i = 0; i < numOfProcess; i++)
     {
         myfile >> arrivalTime >> burstTime;
-        processList.push_back(new Process(i + 1, arrivalTime, burstTime, memory *));
+        processList.push_back(new Process(i + 1, arrivalTime, burstTime, commands, memory));
     }
     myfile.close();
     Clock &clock = Clock::getInstance();
